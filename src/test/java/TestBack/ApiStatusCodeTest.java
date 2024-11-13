80d28d4a22279634d7347c3ae9565fbe77d41e84
Jorge Corrales <jorgecorralesdh@gmail.com>
2024-11-12 21:35:41 -0500

    Backend Test Terminado

--- src/test/java/TestBack/ApiStatusCodeTest.java
+++ src/test/java/TestBack/ApiStatusCodeTest.java
@@ -1,43 +1,132 @@
 package TestBack;
 
-import io.restassured.RestAssured;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import org.junit.jupiter.api.*;
+import reports.ExtentFactory;
 
 import static io.restassured.RestAssured.given;
 
 public class ApiStatusCodeTest {
 
-    private static final String[] URLs = {
-            "https://parabank.parasoft.com/parabank/register.htm",
-            "https://parabank.parasoft.com/parabank/services_proxy/bank/createAccount?customerId=12545&newAccountType=1&fromAccountId=xxxxx",
-            "https://parabank.parasoft.com/parabank/overview.htm",
-            "https://parabank.parasoft.com/parabank/services_proxy/bank/transfer?fromAccountId=13566&toAccountId=13677&amount=xxxxx",
-            "https://parabank.parasoft.com/parabank/services_proxy/bank/accounts/13566/transactions/month/All/type/All"
-    };
+    String baseUrl = "https://parabank.parasoft.com/parabank/";
+    String customerId = "12656";
+    String accountId = "13677";
+    String username = "kira";
+    String password = "123456";
+
+    static ExtentSparkReporter info = new ExtentSparkReporter("target/Report_Backend.html");
+    static ExtentReports extent;
 
     @BeforeAll
-    public static void setup() {
-        RestAssured.baseURI = "https://parabank.parasoft.com/parabank";
+    public static void createReport(){
+        extent = ExtentFactory.getInstance();
+        extent.attachReporter(info);
+    }
+
+    @BeforeEach
+    public void time() throws InterruptedException {
+        Thread.sleep(1000);
     }
 
     @Test
-    public void validateAllUrlsStatus() {
-        for (String url : URLs) {
-            try {
-                System.out.println("Probando URL: " + url);
+    @Order(1)
+    public void registrar() {
+        given()
+                .get(baseUrl + "register.htm")
+                .then()
+                .statusCode(200)
+                .log().body();
+    }
 
-                given()
-                        .get(url)
-                        .then()
-                        .statusCode(200);
+    @Test
+    @Order(2)
+    public void login() {
+        String response = given()
+                .get(baseUrl + "services/bank/" + "login/" + username + "/" + password)
+                .then()
+                .statusCode(200)
+                .log().status()
+                .log().body()
+                .extract().asString();
 
-                System.out.println("URL " + url + " respondió con código 200 OK.");
-            } catch (AssertionError e) {
-                System.out.println("ERROR en URL " + url + ": " + e.getMessage());
-            } catch (Exception e) {
-                System.out.println("Excepción desconocida en URL " + url + ": " + e.getMessage());
-            }
-        }
+        int tagInicio = response.indexOf("<id>") + "<id>".length();
+        int tagFinal = response.indexOf("</id>");
+        customerId = response.substring(tagInicio, tagFinal);
+
+        System.out.println("Customer ID: " + customerId);
+    }
+
+    @Test
+    @Order(3)
+    public void crearCuenta() {
+        String response = given()
+                .get( baseUrl + "services/bank/customers/" + customerId + "/accounts")
+                .then()
+                .statusCode(200)
+                .log().status()
+                .log().body()
+                .extract().asString();
+
+        int tagInicio = response.indexOf("<id>") + "<id>".length();
+        int tagFinal = response.indexOf("</id>");
+        customerId = response.substring(tagInicio, tagFinal);
+
+        System.out.println("Account ID: " + accountId);
+    }
+
+    @Test
+    @Order(4)
+    public void nuevaCuenta() {
+        String response = given()
+                .auth()
+                .basic(username, password)
+                .get( baseUrl + "services_proxy/bank/createAccount?customerId=" + customerId + "&newAccountType=1&fromAccountId=" + accountId)
+                .then()
+                .statusCode(200)
+                .log().status()
+                .log().body()
+                .extract().asString();
+    }
+
+    @Test
+    @Order(5)
+    public void resumenCuenta() {
+        given()
+                .get(baseUrl + "overview.htm")
+                .then()
+                .statusCode(200)
+                .log().status()
+                .log().body();
+    }
+
+    @Test
+    @Order(6)
+    public void descargaFondos () {
+        String amount = "200";
+        given()
+                .get(baseUrl + "/services_proxy/bank/transfer?fromAccountId=" + accountId + "&toAccountId=" + accountId + "&amount=" + amount)
+                .then()
+                .statusCode(200)
+                .log().status()
+                .log().body();
+    }
+
+    @Test
+    @Order(7)
+    public void actividadCuenta (){
+        given()
+                .get(baseUrl + "/services_proxy/bank/accounts/" + accountId + "/transactions/month/All/type/All")
+                .then()
+                .statusCode(200)
+                .log().status()
+                .log().body();
+    }
+
+    @AfterAll
+    public static void saveReport() {
+        extent.flush();
     }
 }
+
+
